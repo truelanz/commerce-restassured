fdc2175263714710e19f29a6249736b9798538eb
truelanz <devalanoliveira@gmail.com>
2025-06-14 15:08:39 -0300

    findAll tests

--- src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dscommerce.controllers;
 
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.ResponseAwareMatcher.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
@@ -30,5 +32,40 @@ public class ProductControllerRA {
             .body("price", is(2190.0F))
             .body("categories.id", hasItems(2,3))
             .body("categories.name", hasItems("Eletronicos", "Computadores"));      
+    }
+
+    @Test
+    public void findAllShouldReturnProductPageWhenNameIsEmpty() {
+        
+        given()
+            .get("/products?sort=name")
+        .then()
+            .statusCode(200)
+            .body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+    }
+
+    @Test
+    public void findAllShouldReturnProductWhenProductNameIsNotEmpty() {
+
+        String productName = "Macbook";
+
+        given()
+            .get("/products?name={productName}", productName)
+        .then()
+            .statusCode(200)
+            .body("content.id[0]", is(3))
+            .body("content.name[0]", equalTo("Macbook Pro"))
+            .body("content.price[0]", is(1250.0F));
+    }
+
+    @Test // Busca todos os produtos que tem o preço maior que 2000.0
+    public void findAllShouldReturnProductWhenPriceGreatherThan2000() {
+
+        given()
+            .get("/products?size=25")
+        .then()
+            .statusCode(200)
+            .body("content.findAll {it.price > 2000.0}.name", hasItems("PC Gamer Foo", "PC Gamer Weed"))
+            .body("content.findAll {it.price > 2000.0}.size()", equalTo(7));
     }
 }
